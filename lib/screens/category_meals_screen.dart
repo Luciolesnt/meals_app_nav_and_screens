aca900fc844a2105e5d2ec4a4257447c910d4002
Lucie Santer <69685489+Luciolesnt@users.noreply.github.com>
2023-03-20 18:44:30 +0100

    Feat(recipe-items): display content + using network images

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../items/meal_item.dart';
+import '../datas/dummy_meals.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   const CategoryMealsScreen({super.key});
@@ -16,15 +18,25 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context)?.settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
+    final categoryMeals = dummyMeals.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('The Recipes : $categoryTitle'),
-      ),
-      body: const Center(
-        child: Text(
-          'The Recipes For the Category!',
+        appBar: AppBar(
+          title: Text('The Recipes : $categoryTitle'),
         ),
-      ),
-    );
+        body: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return MealItem(
+              title: categoryMeals[index].title,
+              imageUrl: categoryMeals[index].imageUrl,
+              duration: categoryMeals[index].duration,
+              complexity: categoryMeals[index].complexity,
+              affordability: categoryMeals[index].affordability,
+            );
+          },
+          itemCount: categoryMeals.length,
+        ));
   }
 }
