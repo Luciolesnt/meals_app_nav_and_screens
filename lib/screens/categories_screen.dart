8bf71d2f001f3dd1aae2b5e2a1f46b5f19c55479
Lucie Santer <69685489+Luciolesnt@users.noreply.github.com>
2023-03-20 15:54:55 +0100

    Feat(categories-screen): render GridView - categories as page

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,17 +1,35 @@
 import 'package:flutter/material.dart';
 
+import '../datas/dummy_data.dart';
+import '../items/category_item.dart';
+
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20),
-      children: const <Widget>[],
+    // Le widget Scafold est indispensble pour wrapper notre screen
+    //(permet de délimiter l'écran en pretant attention au format de la'appareil utilisé par l'uilisateur)
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(15),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: dummyCategories
+            .map(
+              (catDta) => CategoryItem(
+                catDta.title,
+                catDta.color,
+              ),
+            )
+            .toList(),
+      ),
     );
   }
 }
